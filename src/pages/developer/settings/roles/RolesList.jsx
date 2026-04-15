c8fdb7ad8e7a536ef01d92b4d690fb0aa255841c
nItsu69 <alisonmaliksi07@gmail.com>
2026-04-15 13:50:25 +0800

    display

--- src/pages/developer/settings/roles/RolesList.jsx
+++ src/pages/developer/settings/roles/RolesList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import useQueryData from "./../../../../functions/custom-hooks/useQueryData";
 import { apiVersion } from "../../../../functions/functions-general";
+import NoData from "../../../../partials/NoData";
 
 const RolesList = ({ setItemEdit }) => {
   const {
@@ -13,7 +14,43 @@ const RolesList = ({ setItemEdit }) => {
     "roles", //query key
   );
 
-  return <></>;
+  return (
+    <>
+      <div className="relative">
+        <table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Role</th>
+              <th>Description</th>
+              <th>Created</th>
+              <th>Updated</th>
+              <th>Date updated</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataRoles?.count == 0 ? (
+              <tr>
+                <td colSpan="100%" className="p-10">
+                  <NoData />
+                </td>
+              </tr>
+            ) : (
+              dataRoles?.data.map((item, key) => {
+                return (
+                  <tr key={key}>
+                    <td>{key + 1}.</td>
+                    <td>{item.role_name}</td>
+                  </tr>
+                );
+              })
+            )}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default RolesList;
