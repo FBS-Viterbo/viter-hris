352b24ae016649732ccbb01a3575a68acfc096e4
nItsu69 <alisonmaliksi07@gmail.com>
2026-04-20 15:28:45 +0800

    users update

--- src/pages/developer/settings/users/UsersList.jsx
+++ src/pages/developer/settings/users/UsersList.jsx
@@ -19,166 +19,165 @@ import Status from "../../../../partials/Status";
 import ModalArchive from "../../../../partials/modals/ModalArchive";
 import ModalRestore from "../../../../partials/modals/ModalRestore";
 import ModalDelete from "../../../../partials/modals/ModalDelete";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { queryDataInfinite } from "../../../../functions/custom-hooks/queryDataInfinite";
+import { useInView } from "react-intersection-observer";
+import ServerError from "../../../../partials/ServerError";
+import Loadmore from "../../../../partials/Loadmore";
+import SearchBar from "./../../../../partials/SearchBar";
 
-const UsersList = ({ itemEdit, setItemEdit }) => {
+const UsersList = ({ setItemEdit, itemEdit }) => {
   const { store, dispatch } = React.useContext(StoreContext);
+  // page
+  const [page, setPage] = React.useState(1);
+  const [filterData, setFilterData] = React.useState("");
+  const [onSearch, setOnSearch] = React.useState(false);
+  const search = React.useRef({ value: "" });
+  const { ref, inView } = useInView();
+  let counter = 1;
 
+  //use if with loadmore button and search bar
   const {
-    isLoading,
+    data: result,
+    error,
+    fetchNextPage,
+    hasNextPage,
     isFetching,
-    data: dataRoles,
-  } = useQueryData(
-    `${apiVersion}/controllers/developers/settings/roles/roles.php`, //api path file
-    "get", //method request(get,post,put,delete)
-    "roles", //query key
-  );
-
-  const handleEdit = (item) => {
-    dispatch(setIsAdd(true));
-    setItemEdit(item);
-  };
-
-  const handleArchive = (item) => {
-    dispatch(setIsArchive(true));
-    setItemEdit(item);
-  };
-
-  const handleRestore = (item) => {
-    dispatch(setIsRestore(true));
-    setItemEdit(item);
-  };
-
-  const handleDelete = (item) => {
-    dispatch(setIsDelete(true));
-    setItemEdit(item);
-  };
+    isFetchingNextPage,
+    status,
+  } = useInfiniteQuery({
+    queryKey: ["users", search.current.value, store.isSearch, filterData],
+    queryFn: async ({ pageParam = 1 }) =>
+      await queryDataInfinite(
+        ``, // search endpoint
+        `${apiVersion}/controllers/developers/settings/users/page.php?start=${pageParam}`, // list endpoint
+        // store.isSearch || isFilter, // search boolean, // search boolean
+        false,
+        {
+          filterData,
+          searchValue: search?.current?.value,
+        },
+        `post`,
+      ),
+    getNextPageParam: (lastPage) => {
+      if (lastPage.page < lastPage.total) {
+        return lastPage.page + lastPage.count;
+      }
+      return;
+    },
+    refetchOnWindowFocus: false,
+  });
+  React.useEffect(() => {
+    if (inView) {
+      setPage((prev) => prev + 1);
+      fetchNextPage();
+    }
+  }, [inView]);
 
   return (
     <>
-      <div className="relative pt-4 rounded-md">
-        {isFetching && !isLoading && <FetchingSpinner />}
+      <div className="py-5 flex items-center justify-between">
+        <div className="relative">
+          <label htmlFor="">Status</label>
+          <select
+            name="status"
+            id=""
+            value={filterData}
+            onChange={(e) => setFilterData(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="1">Active</option>
+            <option value="0">Inactive</option>
+          </select>
+        </div>
+        <SearchBar
+          search={search}
+          dispatch={dispatch}
+          store={store}
+          result={result?.pages}
+          isFetching={isFetching}
+          setOnSearch={setOnSearch}
+          onSearch={onSearch}
+        />
+      </div>
+      <div className="relative">
+        {status !== "pending" && isFetching && <FetchingSpinner />}
         <table>
           <thead>
             <tr>
               <th>#</th>
               <th>Status</th>
+              <th>Name</th>
+              <th>Email</th>
               <th>Role</th>
-              <th>Description</th>
               <th>Created</th>
-              <th>Date update</th>
-              <th></th>
+              <th>Updated</th>
             </tr>
           </thead>
           <tbody>
-            {isLoading ? (
+            {/* loading screen */}
+            {!error &&
+              (status == "pending" || result?.pages[0]?.count == 0) && (
+                <tr>
+                  <td colSpan="100%" className="p-10">
+                    {status == "pending" ? (
+                      <TableLoading cols={2} count={20} />
+                    ) : (
+                      <NoData />
+                    )}
+                  </td>
+                </tr>
+              )}
+            {/* if request api failed */}
+            {error && (
               <tr>
                 <td colSpan="100%" className="p-10">
-                  <TableLoading cols={2} count={20} />
+                  <ServerError />
                 </td>
               </tr>
-            ) : dataRoles?.count == 0 ? (
-              <tr>
-                <td colSpan="100%" className="p-10">
-                  <NoData />
-                </td>
-              </tr>
-            ) : (
-              dataRoles?.data?.map((item, key) => {
-                return (
-                  <tr key={key}>
-                    <td>{key + 1}.</td>
-                    <td>
-                      <Status
-                        text={`${item.role_is_active == 1 ? "active" : "inactive"}`}
-                      />
-                    </td>
-                    <td>{item.role_name}</td>
-                    <td>{item.role_description}</td>
-                    <td>{formatDate(item.role_created, "--", "short-date")}</td>
-                    <td>{formatDate(item.role_updated, "--", "short-date")}</td>
-                    <td>
-                      <div className="flex items-center gap-3">
-                        {item.role_is_active == 1 ? (
-                          <>
-                            <button
-                              type="button"
-                              className="tooltip-action-table"
-                              data-tooltip="Edit"
-                              onClick={() => handleEdit(item)}
-                            >
-                              <FaEdit />
-                            </button>
-                            <button
-                              type="button"
-                              className="tooltip-action-table"
-                              data-tooltip="Archive"
-                              onClick={() => handleArchive(item)}
-                            >
-                              <FaArchive />
-                            </button>
-                          </>
-                        ) : (
-                          <>
-                            <button
-                              type="button"
-                              className="tooltip-action-table"
-                              data-tooltip="Restore"
-                              onClick={() => handleRestore(item)}
-                            >
-                              <FaTrashRestore />
-                            </button>
-                            <button
-                              type="button"
-                              className="tooltip-action-table"
-                              data-tooltip="Delete"
-                              onClick={() => handleDelete(item)}
-                            >
-                              <FaTrash />
-                            </button>
-                          </>
-                        )}
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })
             )}
+            {/* if request api sucess and data exist then show */}
+            {result?.pages.map((pages, key) => (
+              <React.Fragment key={key}>
+                {pages?.data.map((item, key) => {
+                  return (
+                    <tr key={key}>
+                      <td>{counter++}</td>
+                      <td>
+                        <Status
+                          text={
+                            item.users_is_active == 1 ? "active" : "inactive"
+                          }
+                        />
+                      </td>
+                      <td>
+                        {item.users_first_name}
+                        {item.users_last_name}
+                      </td>
+                      <td>{item.users_email}</td>
+                      <td>{item.role_name}</td>
+                      <td>{formatDate(item.users_created)}</td>
+                      <td>{formatDate(item.users_updated)}</td>
+                    </tr>
+                  );
+                })}
+              </React.Fragment>
+            ))}
           </tbody>
         </table>
+        <div className="loadmore flex justify-center flex-col items-center pb-10">
+          <Loadmore
+            fetchNextPage={fetchNextPage}
+            isFetchingNextPage={isFetchingNextPage}
+            hasNextPage={hasNextPage}
+            result={result?.pages[0]}
+            setPage={setPage}
+            page={page}
+            refView={ref}
+            isSearchOrFilter={store.isSearch || store?.isFilter}
+          />
+        </div>
       </div>
-
-      {store.isArchive && (
-        <ModalArchive
-          mysqlApiArchive={`${apiVersion}/controllers/developers/settings/roles/active.php?id=${itemEdit.role_aid}`}
-          dataItem={itemEdit}
-          msg="Are you sure you want to archive this record?"
-          successMsg={"Successfully archived"}
-          item={itemEdit.role_name}
-          queryKey="roles"
-        />
-      )}
-
-      {store.isRestore && (
-        <ModalRestore
-          mysqlApiRestore={`${apiVersion}/controllers/developers/settings/roles/active.php?id=${itemEdit.role_aid}`}
-          dataItem={itemEdit}
-          msg="Are you sure you want to restore this record?"
-          successMsg={"Successfully restore"}
-          item={itemEdit.role_name}
-          queryKey="roles"
-        />
-      )}
-
-      {store.isDelete && (
-        <ModalDelete
-          mysqlApiDelete={`${apiVersion}/controllers/developers/settings/roles/roles.php?id=${itemEdit.role_aid}`}
-          dataItem={itemEdit}
-          msg="Are you sure you want to delete this record?"
-          successMsg={"Successfully deleted"}
-          item={itemEdit.role_name}
-          queryKey="roles"
-        />
-      )}
     </>
   );
 };
